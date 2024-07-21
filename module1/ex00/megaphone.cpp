c797e1058cffeb0ebc578ca6db63bb54acdf7d93
yoshiddddd <yoshidakerapo@gmail.com>
2024-07-21 11:15:34 +0900

    ex00 done

--- module1/ex00/megaphone.cpp
+++ module1/ex00/megaphone.cpp
@@ -1,6 +1,27 @@
 #include <iostream>
 
+void is_alpha(char *str){
+    int i = 0;
+    while(str[i]){
+        if (str[i] >= 'a' && str[i] <= 'z'){
+            str[i] = str[i] - 32;
+            std:: cout << str[i];
+        }
+        else{
+            std::cout << str[i];
+        }
+        i++;
+    }
+}
+
 int main(int argc, char *argv[]){
-    std::cout << argv[1] << std::endl;
+    if (argc == 1){
+        std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+        return 0;
+    }
+    while(*++argv){
+        is_alpha(*argv);
+    }
+    // std::cout << argv[1] << std::endl;
     return 0;
 }
