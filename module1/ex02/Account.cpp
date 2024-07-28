639542c20bbfcc345ad33a2fdbb359207cb5712b
yoshiddddd <yoshidakerapo@gmail.com>
2024-07-28 10:39:06 +0900

    コンストラクタの作成

---
+++ module1/ex02/Account.cpp
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Account.cpp                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yoshidakazushi <yoshidakazushi@student.    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/28 10:22:47 by yoshidakazu       #+#    #+#             */
+/*   Updated: 2024/07/28 10:37:38 by yoshidakazu      ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Account.hpp"
+
+Account::Account(int initial_deposit){
+    this->_amount = initial_deposit;
+    this->_nbDeposits = 0;
+    this->_nbWithdrawals = 0;
+    this->_accountIndex = Account::_nbAccounts;
+    Account::_nbAccounts++;
+    Account::_totalAmount += initial_deposit;
+    Account::_displayTimestamp();
+    std::cout << "index:" << this->_accountIndex << ";amount:" << this->_amount << ";created" << std::endl;
+}
