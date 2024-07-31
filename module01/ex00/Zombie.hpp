456c4c63855c461fa61727afebfc547760b0e4d6
Kazushi Yoshida <kyoshida@c1r4s8.42tokyo.jp>
2024-07-31 18:11:56 +0900

    zombie create

--- module01/ex00/Zombie.hpp
+++ module01/ex00/Zombie.hpp
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   zombie.hpp                                         :+:      :+:    :+:   */
+/*   Zombie.hpp                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: yoshidakazushi <yoshidakazushi@student.    +#+  +:+       +#+        */
+/*   By: kyoshida <kyoshida@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/31 15:41:48 by yoshidakazu       #+#    #+#             */
-/*   Updated: 2024/07/31 15:43:46 by yoshidakazu      ###   ########.fr       */
+/*   Updated: 2024/07/31 18:08:07 by kyoshida         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,9 +19,8 @@ class Zombie{
     public:
         Zombie();
         ~Zombie();
-        void announce();
-        void set_name(str name);
-
+        void announce(void);
+        // void set_name(str name);
     private:
     str name;
 };
--- module01/ex00/main.cpp
+++ module01/ex00/main.cpp
@@ -3,10 +3,34 @@
 /*                                                        :::      ::::::::   */
 /*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: yoshidakazushi <yoshidakazushi@student.    +#+  +:+       +#+        */
+/*   By: kyoshida <kyoshida@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/31 15:39:51 by yoshidakazu       #+#    #+#             */
-/*   Updated: 2024/07/31 15:39:52 by yoshidakazu      ###   ########.fr       */
+/*   Updated: 2024/07/31 17:56:11 by kyoshida         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+
+#include "Zombie.hpp"
+
+Zombie::Zombie()
+{
+	std::cout << "Zombie is born" << std::endl;
+}
+Zombie::~Zombie()
+{
+	std::cout << "Zombie is dead" << std::endl;
+}
+void Zombie::announce(void)
+{
+	this->name = "Zombie";
+	std::cout << this->name << " BraiiiiiiinnnzzzZ..." << std::endl;
+	
+}
+
+int main(void)
+{
+	Zombie Zombie;
+	Zombie.announce();
+	return 0;
+}
