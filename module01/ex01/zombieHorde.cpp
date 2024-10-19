727dd65b55b5f067455784ad219a3828acae1533
yoshiddddd <yoshidakerapo@gmail.com>
2024-10-19 19:25:55 +0900

    cppからhpp

--- module01/ex01/zombieHorde.cpp
+++ module01/ex01/zombieHorde.cpp
@@ -6,11 +6,11 @@
 /*   By: yoshidakazushi <yoshidakazushi@student.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/14 16:55:35 by kyoshida          #+#    #+#             */
-/*   Updated: 2024/10/15 23:38:13 by yoshidakazu      ###   ########.fr       */
+/*   Updated: 2024/10/19 19:24:29 by yoshidakazu      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "Zombie.cpp"
+#include "Zombie.hpp"
 
 
 Zombie *Zombie::zombieHorde(int N, std::string name)
