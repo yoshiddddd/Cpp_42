46d60f8d6260f3c49b8bea773e80e69155b182f3
yoshiddddd <yoshidakerapo@gmail.com>
2024-11-10 16:52:30 +0900

    init ex03:

---
+++ module01/ex03/Weapon.cpp
@@ -0,0 +1,10 @@
+#include "Weapon.hpp"
+
+Weapon::Weapon(std::string type)
+{
+    this->type = type;
+}
+
+Weapon::~Weapon()
+{
+}
---
+++ module01/ex03/Weapon.hpp
@@ -0,0 +1,16 @@
+#ifndef WEAPON_HPP
+#define WEAPON_HPP
+#include <iostream>
+
+class Weapon
+{
+    public:
+        Weapon(std::string type);
+        ~Weapon();
+        const std::string &getType(void);
+        void setType(std::string type);
+    private:
+        std::string type;
+};
+
+#endif
