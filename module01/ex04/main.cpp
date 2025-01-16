b8185f44c3d3a3a2ee5b1b36f501d8d24dabae62
Kazushi Yoshida <kyoshida@c4r3s8.42tokyo.jp>
2025-01-16 13:13:25 +0900

    makefile

--- module01/ex04/main.cpp
+++ module01/ex04/main.cpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: yoshidakazushi <yoshidakazushi@student.    +#+  +:+       +#+        */
+/*   By: kyoshida <kyoshida@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/09 09:38:09 by yoshidakazu       #+#    #+#             */
-/*   Updated: 2025/01/09 09:38:10 by yoshidakazu      ###   ########.fr       */
+/*   Updated: 2025/01/16 12:25:40 by kyoshida         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ int main(int argc, char **argv)
     if(argc != 4)
     {
         std::cout << "Error: Invalid number of arguments" << std::endl;
-        std::exit(1);
+        return 0;
     }
     Replace(argv[1],argv[2],argv[3]);
 }
