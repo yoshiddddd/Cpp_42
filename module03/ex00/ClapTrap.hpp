dba188c6ebe61b91adc8b780b23ec05cd4b92454
yoshiddddd <yoshidakerapo@gmail.com>
2025-01-20 08:40:06 +0900

    .h create

--- module03/ex00/ClapTrap.hpp
+++ module03/ex00/ClapTrap.hpp
@@ -6,7 +6,7 @@
 /*   By: yoshidakazushi <yoshidakazushi@student.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/18 22:17:17 by yoshidakazu       #+#    #+#             */
-/*   Updated: 2025/01/18 22:17:50 by yoshidakazu      ###   ########.fr       */
+/*   Updated: 2025/01/20 08:39:44 by yoshidakazu      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,21 @@
 #include <string>
 
 class ClapTrap{
-    
-}
+    private:
+        std::string name;
+        int _hit_points;
+        int _energy_points;
+        int _attack_damage;
+    public:
+        ClapTrap();
+        ClapTrap(std::string name);
+        ClapTrap(const ClapTrap &clap);
+        ClapTrap &operator=(const ClapTrap &clap);
+        ~ClapTrap();
+
+        void attack(std::string const & target);
+        void takeDamage(unsigned int amount);
+        void beRepaired(unsigned int amount);
+};
 
 #endif
