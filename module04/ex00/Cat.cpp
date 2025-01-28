7721dea79d86509147549c79d2e484c108018d88
Kazushi Yoshida <kyoshida@c4r1s11.42tokyo.jp>
2025-01-28 12:26:25 +0900

    Cat create

--- module04/ex00/Cat.cpp
+++ module04/ex00/Cat.cpp
@@ -3,15 +3,37 @@
 /*                                                        :::      ::::::::   */
 /*   Cat.cpp                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: yoshidakazushi <yoshidakazushi@student.    +#+  +:+       +#+        */
+/*   By: kyoshida <kyoshida@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/27 22:54:37 by yoshidakazu       #+#    #+#             */
-/*   Updated: 2025/01/27 22:54:45 by yoshidakazu      ###   ########.fr       */
+/*   Updated: 2025/01/28 12:26:12 by kyoshida         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Cat.hpp"
 
-Cat::Cat() : Animal("Cat"){
-    std::cout << "Cat Default constructor called" << std::endl;
+Cat::Cat(void) :Animal() {
+    std::cout << "Cat Default Constructor called" << std::endl;
+}
+
+Cat::Cat(const Cat& cat) {
+    *this = cat;
+    std::cout << "Cat Copy Constructor Called" << std::endl;
+}
+
+Cat &Cat::operator=(const Cat &cat) {
+    if(this != &cat) {
+        this->_type = cat._type;
+    }
+    std::cout << "Cat Copy Assigment operator Called" << std::endl;
+}
+
+Cat::~Cat(void){
+    std::cout << "Cat destructor called" << std::endl;
+}
+
+
+void Cat::makeSound(void) const 
+{
+    std::cout << "meowwwwww" << std::endl;
 }
