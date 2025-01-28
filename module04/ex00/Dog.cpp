fd814c28285a05e233d4f89729ab003e1bef7da1
Kazushi Yoshida <kyoshida@c4r1s11.42tokyo.jp>
2025-01-28 12:28:04 +0900

    Dog create

--- module04/ex00/Dog.cpp
+++ module04/ex00/Dog.cpp
@@ -6,13 +6,34 @@
 /*   By: kyoshida <kyoshida@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/27 22:54:17 by yoshidakazu       #+#    #+#             */
-/*   Updated: 2025/01/28 12:05:50 by kyoshida         ###   ########.fr       */
+/*   Updated: 2025/01/28 12:27:47 by kyoshida         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Dog.hpp"
 
-Dog::Dog() : Animal("Dog"){
-    std::cout << "Dog Default constructor called" << std::endl;
+Dog::Dog(void) :Animal() {
+    std::cout << "Dog Default Constructor called" << std::endl;
 }
 
+Dog::Dog(const Dog& dog) {
+    *this = dog;
+    std::cout << "Dog Copy Constructor Called" << std::endl;
+}
+
+Dog &Dog::operator=(const Dog &dog) {
+    if(this != &dog) {
+        this->_type = dog._type;
+    }
+    std::cout << "Dog Copy Assigment operator Called" << std::endl;
+}
+
+Dog::~Dog(void){
+    std::cout << "Dog destructor called" << std::endl;
+}
+
+
+void Dog::makeSound(void) const 
+{
+    std::cout << "meowwwwww" << std::endl;
+}
